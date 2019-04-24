75cefe45c549d8dc4d190dbe6f8fe183d0a6d259
Felipe Nascimento <felipe.nascimento@vtex.com.br>
2019-04-24 18:23:27 -0300

    Add methods in loadbalancer client

--- loadbalancer.js
+++ loadbalancer.js
@@ -9,6 +9,18 @@ function getClient(region, accessKeyId, secretAccessKey, sessionToken) {
   if (sessionToken) options.sessionToken = sessionToken
   return new AWS.ELB(options);
 }
+function _describeLoadBalancers(names, region, accessKeyId, secretAccessKey, sessionToken) {
+  return new Promise((fulfill, reject) => {
+    const client = getClient(region, accessKeyId, secretAccessKey, sessionToken)
+    var params = {
+      LoadBalancerNames: names
+    };
+    client.describeLoadBalancers(params, function (err, data) {
+      if (err) reject(err, err.stack);
+      else fulfill(data.LoadBalancerDescriptions);
+    });
+  });
+};
 function _describeInstanceHealth(name, region, accessKeyId, secretAccessKey, sessionToken) {
   return new Promise((fulfill, reject) => {
     const client = getClient(region, accessKeyId, secretAccessKey, sessionToken)
@@ -21,11 +33,37 @@ function _describeInstanceHealth(name, region, accessKeyId, secretAccessKey, ses
     });
   });
 };
+exports.describeLoadBalancers = async (names, region, accessKeyId, secretAccessKey, sessionToken, ttl) => {
+  let key = String.toMD5(`loadbalancers_describe_loadbalancers_${names.join('_')}_${region || 'us-east-1'}_${accessKeyId || ''}_${secretAccessKey || ''}_${sessionToken || ''}`)
+  let cached = await redis.get(key);
+  if (cached) return JSON.parse(cached)
+  let loadbalancers = await _describeLoadBalancers(names, region, accessKeyId, secretAccessKey, sessionToken)
+  await redis.set(key, JSON.stringify(loadbalancers), ttl || 90);
+  return loadbalancers
+}
 exports.describeInstanceHealth = async (name, region, accessKeyId, secretAccessKey, sessionToken, ttl) => {
-  let key = String.toMD5(`beanstalk_describe_instance_health_${name}_${region || 'us-east-1'}_${accessKeyId || ''}_${secretAccessKey || ''}_${sessionToken || ''}`)
+  let key = String.toMD5(`loadbalancers_describe_instance_health_${name}_${region || 'us-east-1'}_${accessKeyId || ''}_${secretAccessKey || ''}_${sessionToken || ''}`)
   let cached = await redis.get(key);
   if (cached) return JSON.parse(cached)
   let states = await _describeInstanceHealth(name, region, accessKeyId, secretAccessKey, sessionToken)
   await redis.set(key, JSON.stringify(states), ttl || 90);
   return states
+}
+exports.createLoadBalancerListeners = async (params, region, accessKeyId, secretAccessKey, sessionToken) => {
+  return new Promise((fulfill, reject) => {
+    const client = getClient(region, accessKeyId, secretAccessKey, sessionToken)
+    client.createLoadBalancerListeners(params, function (err, data) {
+      if (err) reject(err, err.stack);
+      else fulfill(data.Listeners);
+    });
+  });
+}
+exports.deleteLoadBalancerListeners = async (params, region, accessKeyId, secretAccessKey, sessionToken) => {
+  return new Promise((fulfill, reject) => {
+    const client = getClient(region, accessKeyId, secretAccessKey, sessionToken)
+    client.deleteLoadBalancerListeners(params, function (err, data) {
+      if (err) reject(err, err.stack);
+      else fulfill(data);
+    });
+  });
 }
