5ed82a15d62e357361d33b872f0aba51cfb503b5
Felipe Nascimento <felipe.nascimento@vtex.com.br>
2019-08-06 20:15:41 -0300

    Remove redis client file

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 require('type-extensions/string')
 const AWS = require("aws-sdk");
-const redis = require('./redis')
-AWS.events.on('retry', function(resp) {
+const IoRedis = require('ioredis')
+const getCache = (key) => {
+  return new Promise((fulfill, reject) => {
+    let client = new IoRedis(process.env.REDIS_PORT, process.env.REDIS_HOST)
+    client.get(key, (err, data) => {
+      client.disconnect()
+      if (err) reject(err)
+      else {
+        fulfill(data)
+      }
+    })
+  })
+}
+const setCache = (key, data, expires) => {
+  return new Promise((fulfill, reject) => {
+    let client = new IoRedis(process.env.REDIS_PORT, process.env.REDIS_HOST)
+    client.set(key, data, 'ex', expires || 60)
+      .then(data => {
+        client.disconnect()
+        fulfill(data)
+      })
+      .catch(err => {
+        client.disconnect()
+        reject(err)
+      })
+  })
+}
+AWS.events.on('retry', function (resp) {
   if (resp.error && resp.error.code === 'Throttling') {
     resp.error.retryable = true;
   }
@@ -22,12 +48,12 @@ class AWSCachedProxy {
           return new Promise((fulfill, reject) => {
             this.client[prop](params, (err, data) => {
               if (err) {
-                if(err.code === 'Throttling') {
+                if (err.code === 'Throttling') {
                   //TODO: LOG KPI for throttling
-                  if(retry < 4)
-                  retry += 1
+                  if (retry < 4)
+                    retry += 1
                   setTimeout(() => {
-                    this[prop](params,retry)
+                    this[prop](params, retry)
                       .then(fulfill)
                       .catch(reject)
                   }, 500 * retry)
@@ -41,10 +67,10 @@ class AWSCachedProxy {
         if (prop.startsWith('get') || prop.startsWith('describe') || prop.startsWith('read')) {
           this[`${prop}Cached`] = async function (params, ttl) {
             let key = `${prop}:${String.toMD5({ token: this.options.sessionToken, params: params })}`
-            let cached = await redis.get(key);
+            let cached = await getCache(key);
             if (cached) return JSON.parse(cached)
             let response = await this[prop](params)
-            if (response) redis.set(key, JSON.stringify(response), ttl || 90);
+            if (response) setCache(key, JSON.stringify(response), ttl || 60);
             return response
           }
         }
@@ -53,3 +79,4 @@ class AWSCachedProxy {
   }
 }
 module.exports = AWSCachedProxy
+
