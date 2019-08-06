d8c4dce6a5302bb248f1ec5534733ee273816c7b
Felipe Nascimento <felipe.nascimento@vtex.com.br>
2019-08-06 17:04:55 -0300

    Add index with only one client

---
+++ index.js
@@ -0,0 +1,54 @@
+const AWS = require("aws-sdk");
+const redis = require('./redis')
+AWS.events.on('retry', function(resp) {
+  if (resp.error && resp.error.code === 'Throttling') {
+    resp.error.retryable = true;
+  }
+});
+class AWSCachedProxy {
+  constructor(AWSClient, region = "us-east-1", accessKeyId, secretAccessKey, sessionToken) {
+    this.AWS = AWS
+    this.options = {
+      region: region
+    };
+    if (accessKeyId) this.options.accessKeyId = accessKeyId
+    if (secretAccessKey) this.options.secretAccessKey = secretAccessKey
+    if (sessionToken) this.options.sessionToken = sessionToken
+    this.client = new AWSClient(this.options)
+    for (let prop in this.client) {
+      if (typeof this.client[prop] === 'function') {
+        this[prop] = function (params, retry = 0) {
+          return new Promise((fulfill, reject) => {
+            this.client[prop](params, (err, data) => {
+              if (err) {
+                if(err.code === 'Throttling') {
+                  //TODO: LOG KPI for throttling
+                  if(retry < 4)
+                  retry += 1
+                  setTimeout(() => {
+                    this[prop](params,retry)
+                      .then(fulfill)
+                      .catch(reject)
+                  }, 500 * retry)
+                }
+                else reject(err)
+              }
+              else fulfill(data)
+            })
+          })
+        }
+        if (prop.startsWith('get') || prop.startsWith('describe') || prop.startsWith('read')) {
+          this[`${prop}Cached`] = async function (params, ttl) {
+            let key = `${prop}:${String.toMD5({ token: this.options.sessionToken, params: params })}`
+            let cached = await redis.get(key);
+            if (cached) return JSON.parse(cached)
+            let response = await this[prop](params)
+            if (response) redis.set(key, JSON.stringify(response), ttl || 90);
+            return response
+          }
+        }
+      }
+    }
+  }
+}
+module.exports = AWSCachedProxy
